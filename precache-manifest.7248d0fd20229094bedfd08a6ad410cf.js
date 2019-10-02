d5e07cabf4bffa1846d3d627e45b4828a48189a4
MJDC <kpjob555@gmail.com>
2019-10-02 17:19:00 +0700

    Updates

---
+++ precache-manifest.7248d0fd20229094bedfd08a6ad410cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8759c11a22a37c2aeea3d8b50ed77ac1",
+    "url": "/Sum-Average-SD/index.html"
+  },
+  {
+    "revision": "f6ffba795fbe42e350e6",
+    "url": "/Sum-Average-SD/static/css/main.b040eca4.chunk.css"
+  },
+  {
+    "revision": "6eb8c17ed81e98e0028e",
+    "url": "/Sum-Average-SD/static/js/2.a113c019.chunk.js"
+  },
+  {
+    "revision": "f6ffba795fbe42e350e6",
+    "url": "/Sum-Average-SD/static/js/main.a6bf0906.chunk.js"
+  },
+  {
+    "revision": "63519d5b3fc20eae71c5",
+    "url": "/Sum-Average-SD/static/js/runtime-main.59b64961.js"
+  }
+]);
